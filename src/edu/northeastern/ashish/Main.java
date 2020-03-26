f95da53ec871e9b9b33a72faaef028e895330c2c
Ashish <happycoder29@gmail.com>
2020-03-25 18:43:00 -0700

    Chec king in code for few more questions

--- src/edu/northeastern/ashish/Main.java
+++ src/edu/northeastern/ashish/Main.java
@@ -1,6 +1,7 @@
 package edu.northeastern.ashish;
 
 import java.lang.reflect.Array;
+import java.sql.PreparedStatement;
 import java.util.*;
 
 public class Main {
@@ -14,15 +15,28 @@ public class Main {
 //
 //        System.out.println(getLongestPalindromeSubString(str));
 
-        ArrayList<Interval> arrayList = new ArrayList<>();
-        arrayList.add(new Interval(1,5));
-        arrayList.add(new Interval(3,7));
-        arrayList.add(new Interval(2,4));
-        arrayList.add(new Interval(8,15));
+//        ArrayList<Interval> arrayList = new ArrayList<>();
+//        arrayList.add(new Interval(1,5));
+//        arrayList.add(new Interval(3,7));
+//        arrayList.add(new Interval(2,4));
+//        arrayList.add(new Interval(8,15));
+//
+//        mergeIntervals(arrayList);
 
-        mergeIntervals(arrayList);
+//        int[] arr = {-4,-2,0, 1,3,5};
+//        int[] squares = squareOfSortedArray(arr);
 
+//        int[] arr = {5,6,1,2,3,4};
+//        findMinInSortedRotated(arr);
+//        System.out.println(findMinInSortedRotated(arr));
+//        int[] arr1 = {1,3,5,7};
+//
+//        int[] arr2 = {2,4,6,8,9,10};
+//
+//        int[] result = mergeSortedArrays(arr1, arr2);
+//        System.out.println("");
 
+        System.out.println(isInterleaved("ACEF", "BD", "ABEDCF"));
 
 
     }
@@ -198,4 +212,145 @@ public class Main {
         }
     }
 
-}
+    private static int[] squareOfSortedArray(int[] arr){
+        int[] squares = new int[arr.length];
+
+        int start = 0;
+        int end = arr.length -1;
+        int index = arr.length -1;
+
+        while(start <= end){
+            int sq1 = arr[start] * arr[start];
+            int sq2 = arr[end] * arr[end];
+
+            if( sq1 > sq2){
+                squares[index --] = sq1;
+                start++;
+            }else{
+                squares[index --] = sq2;
+                end--;
+            }
+        }
+
+        return  squares;
+    }
+
+
+    private static int bittonicPeak(int[] arr){
+        int start = 0;
+        int end = arr.length -1;
+        while(start < end){
+
+            int mid = (start + end)/2;
+
+            if(arr[mid] < arr[mid+1]){
+                start = mid +1;
+            }else{
+                end = mid;
+            }
+        }
+        return  arr[start];
+    }
+
+    private static void swap(int[] arr, int start, int end){
+
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+    }
+
+    private static void reverse(int[] arr, int start, int end){
+        while(start < end){
+            swap(arr, start,end);
+            start++;
+            end--;
+        }
+    }
+
+    private static void rotateArray(int[] arr, int k){
+        k = k % arr.length;
+
+        reverse(arr, 0, arr.length-k -1);
+        reverse(arr, arr.length-k, arr.length-1);
+
+        reverse(arr, 0, arr.length -1);
+
+    }
+
+
+    private static int findMinInSortedRotated(int[] arr){
+        int start = 0;
+        int end = arr.length -1;
+
+        if(arr[start] < arr[end])
+            return arr[start];
+        while(start < end -1){
+
+            int mid = (start + end)/2;
+            if(arr[start] > arr[mid]){
+                end = mid;
+            }else{
+                start = mid;
+            }
+        }
+
+        return  arr[end];
+    }
+
+
+    private static int[] mergeSortedArrays(int[] arr1, int[] arr2){
+        int[] result = new int[arr1.length  + arr2.length];
+        int ptr1 = 0;
+        int ptr2 = 0;
+        int index = 0;
+        while(ptr1 < arr1.length && ptr2 < arr2.length){
+            if(arr1[ptr1] < arr2[ptr2]){
+                result[index++] = arr1[ptr1++];
+            }else{
+                result[index++] = arr2[ptr2++];
+            }
+        }
+
+        while(ptr1 < arr1.length){
+            result[index++] = arr1[ptr1++];
+        }
+
+        while(ptr2 < arr2.length){
+            result[index++] = arr2[ptr2++];
+        }
+
+        return  result;
+    }
+
+
+    private static  boolean isInterleaved(String str1, String str2, String str3){
+
+        return  isInterleaved(str1, str2, str3, "", 0, 0);
+    }
+
+    private static  boolean isInterleaved(String str1,
+                                          String str2,
+                                          String str3,
+                                          String result,
+                                          int ptr1,
+                                          int ptr2){
+
+        if( result.equals(str3) && ptr1 == str1.length() && ptr2 == str2.length())
+            return  true;
+
+        boolean answer = false;
+        if( ptr1 < str1.length()){
+            answer = answer || isInterleaved(str1, str2, str3, result + str1.charAt(ptr1),ptr1 +1, ptr2);
+        }
+        if( ptr2 < str2.length()){
+            answer = answer || isInterleaved(str1, str2, str3, result + str2.charAt(ptr2),ptr1 , ptr2 +1);
+        }
+
+        return  answer;
+    }
+
+
+
+
+
+    }
